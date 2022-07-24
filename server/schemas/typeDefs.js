135afd3a40543c1ee186a50bcb98139db26aaa7c
Alex Vazquez <avazque008@outlook.com>
2022-07-23 22:13:53 -0400

    created queries and mutations files

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,9 +38,9 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login (email: String!, password: String!): Auth
+        loginUser (email: String!, password: String!): Auth
         addUser (username: String!, email: String!, password: String!): Auth
-        saveBook (input: SaveBookInput): User
+        saveBook (input: SaveBookInput!): User
         removeBook (bookId: String!): User
     }
 `;
